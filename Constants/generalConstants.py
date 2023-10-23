119fedaa841c3ce2d80a1670b8a8fcd7d1f54403
Juan David Moreno Alfonso <juandavid@cressco.dev>
2023-10-23 18:23:36 -0500

    add pictures to constatns

--- Constants/generalConstants.py
+++ Constants/generalConstants.py
@@ -21,7 +21,7 @@ INFO_ENTITIES = {
     2: "Abusador amo",
     3: "Gato malvado",
     4: "Muriel",
-    5: "Obstaculo"
+    5: "Obstaculo",
 }
 
 INFO_ENTITIES_IMAGES = {
@@ -31,4 +31,7 @@ INFO_ENTITIES_IMAGES = {
     3: "findMyOwner\Images\katz.png",
     4: "findMyOwner\Images\muriel.png",
     5: "findMyOwner\Images/black.jpg",
+    6: "findMyOwner\Images/abusadorEfecto.jpg",
+    7: "findMyOwner\Images\katzEfecto.jpg",
+    8: "findMyOwner\Images\murielEfecto.jpg"
 }
