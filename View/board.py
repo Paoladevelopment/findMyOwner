16714b41182e6fa9e0effda336525281ea655345
Juan David Moreno Alfonso <juandavid@cressco.dev>
2023-10-25 10:05:20 -0500

    add some effects

--- View/board.py
+++ View/board.py
@@ -27,7 +27,7 @@ class GameBoardApp:
                 img = ImageTk.PhotoImage(resized_image)
                 cell_label = tk.Label(
                     self.root,
-                    borderwidth=1,
+                    borderwidth=0,
                     relief="solid",
                     background="white",
                     image=img,
@@ -51,7 +51,7 @@ class GameBoardApp:
                 img = ImageTk.PhotoImage(resized_image)
                 cell_label = tk.Label(
                     self.root,
-                    borderwidth=1,
+                    borderwidth=0,
                     relief="solid",
                     background="white",
                     image=img,
@@ -65,6 +65,7 @@ class GameBoardApp:
         if self.step < len(self.game_positions):
             new_board = copy.deepcopy(self.game_board)
             positions = self.game_positions
+            time = 250
 
             new_position = positions[self.step]
             current_value = new_board[new_position[0]][new_position[1]]
@@ -73,6 +74,7 @@ class GameBoardApp:
             if current_value in value_mapping:
                 new_value, condition_value = value_mapping[current_value]
                 new_board[new_position[0]][new_position[1]] = new_value
+                time = 500
                 if self.step - 1 and not any(
                     new_board[positions[self.step - 1][0]][positions[self.step - 1][1]]
                     == val
@@ -94,7 +96,7 @@ class GameBoardApp:
                     
             self.update_board(new_board)
             self.step += 1
-            self.root.after(500, self.update_step)
+            self.root.after(time, self.update_step)
 
     def start_travel(self):
         self.step = 0
